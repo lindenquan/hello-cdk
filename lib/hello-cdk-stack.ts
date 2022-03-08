fe934e0bb3015954caf643bf69b16635c95c3610
Linden Quan <linden.quan@resmed.com>
2022-03-07 21:47:27 -0400

    update

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
